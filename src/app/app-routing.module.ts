471ce55ce756d1956ea8b8c8a2746e82263198e6
mamjerez <correomam@gmail.com>
2019-04-18 08:45:55 +0200

    Añado ingresos por economico

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { IndiceComponent } from './indice/indice.component';
 import { AboutComponent } from './about/about.component';
+import { IngresosPorEconomicoComponent } from './ingresos/ingresos-por-economico/ingresos-por-economico.component';
 import { GastosPorProgramaComponent } from './gastos/gastos-por-programa/gastos-por-programa.component';
 import { GastosPorEconomicoComponent } from './gastos/gastos-por-economico/gastos-por-economico.component';
 
 const app_routes: Routes = [
     { path: 'home', component: IndiceComponent },
     { path: 'about', component: AboutComponent },
+    { path: 'IngresosPorEconomico', component: IngresosPorEconomicoComponent },
     { path: 'GastosPorPrograma', component: GastosPorProgramaComponent },
     { path: 'GastosPorEconomico', component: GastosPorEconomicoComponent },
     { path: '**', pathMatch: 'full', redirectTo: 'home' }
